195924ae3132fce1aa78dfc8777a4fc519f5ab7d
leeminju <leemj4090@naver.com>
2018-10-16 11:07:15 +0900

    Add files via upload

---
+++ 계산기/계산기/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace 계산기
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtValue1 = new System.Windows.Forms.TextBox();
+            this.txtValue2 = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtValue1
+            // 
+            this.txtValue1.Location = new System.Drawing.Point(100, 64);
+            this.txtValue1.Name = "txtValue1";
+            this.txtValue1.Size = new System.Drawing.Size(100, 21);
+            this.txtValue1.TabIndex = 0;
+            this.txtValue1.Text = "0";
+            this.txtValue1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtValue2
+            // 
+            this.txtValue2.Location = new System.Drawing.Point(274, 64);
+            this.txtValue2.Name = "txtValue2";
+            this.txtValue2.Size = new System.Drawing.Size(100, 21);
+            this.txtValue2.TabIndex = 1;
+            this.txtValue2.Text = "0";
+            this.txtValue2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("굴림", 13F);
+            this.lblResult.Location = new System.Drawing.Point(459, 64);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(18, 18);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "0";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(74, 143);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Location = new System.Drawing.Point(176, 143);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(75, 23);
+            this.btnSub.TabIndex = 4;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Location = new System.Drawing.Point(278, 143);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(75, 23);
+            this.btnMul.TabIndex = 5;
+            this.btnMul.Text = "*";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Location = new System.Drawing.Point(380, 143);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(75, 23);
+            this.btnDiv.TabIndex = 6;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 251);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtValue2);
+            this.Controls.Add(this.txtValue1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtValue1;
+        private System.Windows.Forms.TextBox txtValue2;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+    }
+}
+
---
+++ 계산기/계산기/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 계산기
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*이벤트 핸들러 -함수인데 특정 이벤트가 발생할 떄
+         * 이벤트에 의해 연동되어 호출된다.
+         * 이벤트 프로그래밍 방식-GUI(Graphic User Interface) 방식
+         * 키보드로 데이터를 입력하는 방식이 아니라 컴퓨터로 들어오는 모든 기계적 움직임을 이벤트라고 보고
+         * 마우스클릭,키누름, 마우스더블클릭,마우스 드래그 버튼 클릭 등을 모두 상수로 저장해 놓고 이러한 이벤트가
+         * 발생하면 이벤트와 연동된 함수들(이벤트 핸들러)를 호출해서 처리하는 방식이다.
+         * 프로그램에서 처리하지 않는 이벤트는 기복적으로 시스템에 처리하도록 되어있다
+         * 이벤트 핸들러는 C#에서는 대리자나를 개념을 통해 구현되어 있다.
+         * 이미 만들오진 대리자를 이용해 호출하기 때문데 함수의 모습을 수정할 수 없다.
+         * 매개변수도 이미 다 지정되어 있는데 이벤트마다 약간씩 다르다*/
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n1 = int.Parse(txtValue1.Text);
+                int n2 = int.Parse(txtValue2.Text);
+
+                lblResult.Text = String.Format("{0}", n1 + n2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n1 = int.Parse(txtValue1.Text);
+                int n2 = int.Parse(txtValue2.Text);
+
+                lblResult.Text = String.Format("{0}", n1 - n2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnMul_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n1 = int.Parse(txtValue1.Text);
+                int n2 = int.Parse(txtValue2.Text);
+
+                lblResult.Text = String.Format("{0}", n1 * n2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n1 = int.Parse(txtValue1.Text);
+                int n2 = int.Parse(txtValue2.Text);
+                lblResult.Text = String.Format("{0}", n1 / n2);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
